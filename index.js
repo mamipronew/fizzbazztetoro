0bc1802cb1cf84d713e6e48b8bf67312f354abb6
mamipronew <gotohanya@yahoo.co.jp>
2020-08-18 21:43:50 +0900

    hogehoge

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const gameObj = {
 
 
 };
-
-let moveX = 0;
+const MaxY = gameObj.gritTate - 1;
+let moveX = 2;
 let moveY = 0;
 
 let screenWidth = gameObj.gritYoko * gameObj.itemWidth;
 let screenHeight = gameObj.gritTate * gameObj.itemWidth;
-let itemArry = [...Array(100).keys()] ;//0から9まで入った配列を作る
+let itemArry = [...Array(99).keys()] ;//配列を作る
 
 //fieldArrayは動かないけど消去される。tblArrayは動くけど消去されない。
 //　tblArray→fieldArrayにする
@@ -38,6 +38,8 @@ let fieldArray = [
   [0,0,0,0,0]
 ];
 
+let copyArray = [];
+
 let tblArray = [...Array(gameObj.gritTate).keys()];
 for (let index = 0; index < tblArray.length; index++) {
   tblArray[index] = new Array(gameObj.gritYoko).fill(0);
@@ -50,22 +52,24 @@ function drawBlock(x, y, num) {
   let blockSize = gameObj.itemWidth;
   let px = x * blockSize;
        let py = y * blockSize;
-          ctx.fillStyle = "red";
+          ctx.fillStyle = "#A22";
         ctx.fillRect(px, py, blockSize, blockSize);
         ctx.strokeStyle = "black";
-        ctx.fillText(num, px, py, blockSize, blockSize);
         ctx.strokeRect(px, py, blockSize, blockSize); 
+        ctx.fillStyle = "white";
+        ctx.fillText(num, px + blockSize /2, py + blockSize/2, blockSize, blockSize);
+       
 
 }
 
 
-tblArray[1][2] = 1;
+tblArray[0][0] = 1;
 let num = selectNum();
 drawAll(num);
 
 
 function drawAll(num) {
-
+ 
   ctx.clearRect(0, 0, screenWidth, screenHeight);
   for (let y = 0; y < gameObj.gritTate ; y++) {
     for (let x = 0; x < gameObj.gritYoko; x++) {
@@ -155,6 +159,47 @@ document.onkeydown = function(e)
 }
 
 
+   
+
+  
+  
+
+
+
+
+
+
+function deleteChecker(x,y ,num) {
+  if (num % 15 == 0) {
+    
+    console.log("FizzBUZZ！");
+  } else if(num % 5 == 0)
+    { 
+
+
+
+
+
+      
+}
+ 
+  else if (num % 3 == 0 && y != 7) {
+    fieldArray[0][x] =0;
+    fieldArray[1][x] =0;
+    fieldArray[2][x] =0;
+    fieldArray[3][x] =0;
+    fieldArray[4][x] =0;
+    fieldArray[5][x] =0;
+    fieldArray[6][x] =0;
+    fieldArray[7][x] =0;
+  
+    
+  }
+
+  
+}
+
+
   //作りかけ
 //x, yの動きの幅を引数に取るのでmx,my
 function checkMove(mx, my) {
@@ -167,11 +212,14 @@ let ny = 0;
         nx = moveX + mx +x;
         ny = moveY + my +y;
         
-        if(fieldArray[ny][nx] ||
+        if(
           nx < 0 ||
             ny < 0 ||
             nx > gameObj.gritYoko -1 ||
-            ny > gameObj.gritTate -1 ){
+            ny > gameObj.gritTate -1 ||
+            fieldArray[ny][nx] 
+            
+            ){
         return false;
         }
 
@@ -192,6 +240,10 @@ function selectNum() {
     
   }
 
+
+
+
+
   function checkBottom() {
     for (let y = 0; y < gameObj.gritTate ; y++) {
       for (let x = 0; x < gameObj.gritYoko; x++) {
@@ -202,22 +254,19 @@ let ny = 0;
           ny = moveY + y;
 
 
-          if( ny >= gameObj.gritTate -1){
+          if(ny >= gameObj.gritTate -1 || fieldArray[ny + 1][nx]){
             
             fieldArray[ny][nx] = num;
 
-          if (num % 15 == 0 
-            
-            ) {
-            console.log("FizzBUZZ");
-            
-          } 
+            deleteChecker(nx,ny,num);
+
+        
 
             
             
             num = selectNum();
-            console.log(num);
-          moveX = 0;
+           ;
+          moveX = 2;
           moveY = 0;
             drawAll(num) ;
           
---
+++ temp.js
@@ -0,0 +1,110 @@
+//作りかけ
+let lineCount = [];
+function checkLine() {
+  for (let y = 0; y < gameObj.gritTate; y++) {
+    //横の列一列を調べるループだから。ここでフラグを建てる
+    let flag = true;
+      for (let x = 0; x < gameObj.gritYoko; x++) {
+        //一列に一個でも空きがあったら
+        if(fieldArray[y][x] == 0)
+        {
+        flag = false;
+        //falseにしてこのループを抜ける
+        
+        break;
+
+      }
+
+lineCount.push(y);
+     
+    
+      }
+      if (flag) {
+        console.log(lineCount);
+        copyArray = fieldArray;
+        console.log(copyArray);
+          for (let nx = 0; nx < gameObj.gritYoko; nx++) {
+            
+            fieldArray[y][nx] = 0;
+        }
+
+        for (let ny = y; 1 > ny; ny--) {
+          for (let nx = 0; nx < gameObj.gritYoko; nx++) {
+            
+            fieldArray[ny][nx] = copyArray[ny - 1][nx];
+           
+        }
+          
+        }
+        for (let nx = 0; nx < gameObj.gritYoko; nx++) {
+            
+          fieldArray[0][nx] = 0;
+         
+      }
+        
+      }
+      
+       
+     }
+  
+  
+}
+
+
+
+      
+copyArray = fieldArray;
+checkLine();
+
+
+console.log(copyArray);
+　　　　lineDelCopy(tempY);
+tempY = 0;
+
+let tempY = 0;
+function checkLine(){
+  
+  
+ 
+for (let ny = 0; ny < 8; ny++) {
+    if (fieldArray[ny][0] != 0 &&
+      fieldArray[ny][1] != 0 &&
+      fieldArray[ny][2] != 0 &&
+      fieldArray[ny][3] != 0 &&
+      fieldArray[ny][4] != 0 
+   
+      ){ tempY = ny;
+        break; }
+}
+
+      
+    }
+
+function lineDelCopy(tempY){
+  if(tempY == 0) return;
+
+
+fieldArray[tempY][0] = 0;
+  fieldArray[tempY][1] = 0;
+  fieldArray[tempY][2] = 0;
+  fieldArray[tempY][3] = 0;
+  fieldArray[tempY][4] = 0;
+
+  for (let ny = 1; ny < tempY; ny++) {
+    for (let nx = 0; nx < gameObj.gritYoko; nx++) {
+      fieldArray[ny][nx] = copyArray[ny -1][nx];
+      fieldArray[0][nx] = 0;
+    }
+    
+  }
+
+  tempY = 0;
+
+
+
+
+
+
+  
+}
+    
